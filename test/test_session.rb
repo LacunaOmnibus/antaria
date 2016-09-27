4ab0e1bcd65122baf1599abca2d158b8e6f6b8cd
Eric MSP Veith <eveith@gnyu-linux.org>
2016-09-27 21:05:00 +0200

    Complete test case for Antaria::Session, using Webmock

--- test/test_session.rb
+++ test/test_session.rb
@@ -1,22 +1,67 @@
+require 'json'
 require 'minitest/autorun'
-require 'yaml'
-require 'antaria'
 
 
 describe Antaria::Session do
   before do
-    File.open 'config.yml' do |file|
-      @config = YAML.load file.read
+    @session = Antaria::Session.new(
+      'empire_name',
+      'empire_password',
+      server_uri: 'https://example.com')
+  end
+
+  describe "uri_for" do
+    it "must build the corrent module URI" do
+      @session.uri_for('mod').must_equal URI('https://example.com/mod')
     end
-    @session = Antaria::Session.new @config['empire'], @config['password']
   end
 
   describe "when asked to log in and out" do
-    it "must log in and out again" do
+    it "must log in" do
+      stub_request(:post, /empire\Z/)
+        .to_return body: File.new('test/webmock/empire-login-successful.json')
+
       @session.login.must_equal true
       @session.logged_in?.must_equal true
-      @session.logout.must_equal true
+
+      assert_requested :post, "https://example.com/empire",
+        body: /method.*?login/
+    end
+
+    it "must log in automagically" do
+      stub_request(:post, /foo\Z/)
+        .to_return(status: 404, body: {'error' => {'code' => 1006}}.to_json)
+        .to_return(status: 200, body: '{ }')
+      stub_request(:post, /empire\Z/)
+        .to_return body: File.new('test/webmock/empire-login-successful.json')
       @session.logged_in?.must_equal false
+      @session.api_call 'foo', 'bar', %w[baz, quux]
+      @session.logged_in?.must_equal true
+      assert_requested :post, /empire\Z/,
+        body: /method.*?login/
+    end
+
+    it "must log out" do
+      stub_request(:post, /empire\Z/)
+        .to_return body: File.new('test/webmock/empire-login-successful.json')
+
+      @session.login
+      @session.logout
+
+      stub_request(:post, /empire\Z/)
+        .with(body: /method\":\"logout/)
+        .to_return(status: 200, body: "", headers: {})
+      assert_requested :post, "https://example.com/empire",
+        body: /method.*?logout/
+    end
+  end
+
+  describe "it offers a game object" do
+    it "returns an initialized Empire object" do
+      stub_request(:post, /empire\Z/)
+        .to_return body: File.new('test/webmock/empire-login-successful.json')
+      e = @session.empire
+      e.name.must_equal 'Antaria'
     end
   end
 end
